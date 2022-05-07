35e898c1932fbf3af8fdeda55fd2df230f935904
Sagar <arsagar8888@gmail.com>
2022-05-07 13:54:55 +0530

    whole notes app v1

--- index.js
+++ index.js
@@ -1,42 +1,7 @@
-// const http = require('http')
-// let notes = [
-//     {
-//       id: 1,
-//       content: "HTML is easy",
-//       date: "2022-05-30T17:30:31.098Z",
-//       important: true
-//     },
-//     {
-//       id: 2,
-//       content: "Browser can execute only Javascript",
-//       date: "2022-05-30T18:39:34.091Z",
-//       important: false
-//     },
-//     {
-//       id: 3,
-//       content: "GET and POST are the most important methods of HTTP protocol",
-//       date: "2022-05-30T19:20:14.298Z",
-//       important: true
-//     }
-//   ]
-
-// const app = http.createServer((req,res)=>{
-//     res.writeHead(200,{'ContentType':'application/json'})
-//     res.end(JSON.stringify(notes))
-// })
-
-// const app = http.createServer((request,response)=>{
-//     response.writeHead(200,{'Content-Type':'text/plain'})
-//     response.end('Hello world')
-// })
-
-// const PORT = 3001
-// app.listen(PORT)
-// console.log(`Server running on port${PORT}`)
-
 const express = require('express')
 const cors = require('cors')
 const app = express()
+app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
 let notes = [
@@ -61,13 +26,8 @@ let notes = [
   },
 ]
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello world!</h1>')
-})
-
 app.get('/api/notes', (request, response) => {
   response.json(notes)
-  console.log(notes)
 })
 
 app.get('/api/notes/:id', (request, response) => {
